5c94d02f508c2cbc872989f8f56d3e93fdc0317d
Fredrik Vraalsen <fvr@knowit.no>
2013-05-23 23:05:38 +0200

    Added toString, hashCode and equals to Sak entity

--- felles/src/main/java/no/knowit/backtobasics/domene/Sak.java
+++ felles/src/main/java/no/knowit/backtobasics/domene/Sak.java
@@ -34,4 +34,48 @@ public class Sak {
         this.opprettet = opprettet;
     }
 
+    @Override
+    public String toString() {
+        return "Sak(" + id + ", " + tittel + ", " + opprettet + ")";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((opprettet == null) ? 0 : opprettet.hashCode());
+        result = prime * result + ((tittel == null) ? 0 : tittel.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Sak other = (Sak) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (opprettet == null) {
+            if (other.opprettet != null)
+                return false;
+        } else if (other.opprettet == null) {
+            return false;
+        } else if (opprettet.getTime() != other.opprettet.getTime())
+            return false;
+        if (tittel == null) {
+            if (other.tittel != null)
+                return false;
+        } else if (!tittel.equals(other.tittel))
+            return false;
+        return true;
+    }
+
 }
