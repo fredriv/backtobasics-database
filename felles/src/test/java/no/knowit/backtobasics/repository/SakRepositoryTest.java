85c3b18a0ec68d8e4f6c1ff705cd8ba0eddf54f5
Fredrik Vraalsen <fvr@knowit.no>
2013-05-23 23:06:47 +0200

    Added some debug logging to repository tests

--- felles/src/test/java/no/knowit/backtobasics/repository/SakRepositoryTest.java
+++ felles/src/test/java/no/knowit/backtobasics/repository/SakRepositoryTest.java
@@ -7,17 +7,21 @@ import java.util.Date;
 
 import no.knowit.backtobasics.domene.Sak;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 public abstract class SakRepositoryTest {
 
+    private Logger log = Logger.getLogger(SakRepositoryTest.class);
+
     protected abstract SakRepository getSakRepository();
-    
+
     @Test
     public void opprett_sak_med_tittel() {
         Date start = new Date();
         Sak sak = getSakRepository().opprettSak("tittel");
-        
+        log.debug("Opprettet sak: " + sak);
+
         assertThat(sak, is(not(nullValue())));
         assertThat(sak.getTittel(), is("tittel"));
         assertThat(sak.getId(), is(greaterThan(0)));
@@ -28,23 +32,25 @@ public abstract class SakRepositoryTest {
     public void opprett_sak_med_tom_tittel() {
         Date start = new Date();
         Sak sak = getSakRepository().opprettSak("");
-        
+        log.debug("Opprettet sak: " + sak);
+
         assertThat(sak, is(not(nullValue())));
         assertThat(sak.getId(), is(not(nullValue())));
         assertThat(sak.getId(), is(greaterThan(0)));
         assertThat(sak.getTittel(), is(""));
         assertThat(sak.getOpprettet().getTime(), is(greaterThanOrEqualTo(start.getTime())));
     }
-    
+
     @Test
     public void hent_sak() {
         Date start = new Date();
         SakRepository sakRepository = getSakRepository();
         Sak sak = sakRepository.opprettSak("tittel");
-        
+
         Integer id = sak.getId();
         sak = sakRepository.hentSak(id);
-        
+        log.debug("Hentet sak: " + sak);
+
         assertThat(sak, is(not(nullValue())));
         assertThat(sak.getId(), is(equalTo(id)));
         assertThat(sak.getTittel(), is("tittel"));
